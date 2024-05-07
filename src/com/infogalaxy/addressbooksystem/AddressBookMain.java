805a71db2a823009df295b84f440505674b33034
NikhilPatil07 <patilnikhil181804@gmail.com>
2024-05-07 18:33:10 +0530

    [MODIFIED]: Nikhil - UC-2 Completed is Add Contact

--- src/com/infogalaxy/addressbooksystem/AddressBookMain.java
+++ src/com/infogalaxy/addressbooksystem/AddressBookMain.java
@@ -1,9 +1,50 @@
 package com.infogalaxy.addressbooksystem;
 
+import java.util.Scanner;
+
 public class AddressBookMain 
 {
+	Scanner scanner = new Scanner(System.in);
+	Contact contact = new Contact();
+	
+	public void addContact()
+	{
+		System.out.println("Enter the First Name :");
+		contact.setFirstName(scanner.next());
+		System.out.println("Enter the Last Name :");
+		contact.setLastName(scanner.next());
+		System.out.println("Enter the Address :");
+		contact.setAddress(scanner.next());
+		System.out.println("Enter the City :");
+		contact.setCity(scanner.next());
+		System.out.println("Enter the State :");
+		contact.setState(scanner.next());
+		System.out.println("Enter the Zip Code :");
+		contact.setZip(scanner.next());
+		System.out.println("Enter the Phone No :");
+		contact.setPhoneNo(scanner.next());
+		System.out.println("Enter the Email :");
+		contact.setEmail(scanner.next());
+	}
+	
+	void displayContact()
+	{
+		System.out.println(" First Name :"+contact.getFirstName());
+		System.out.println(" Last Name :"+contact.getLastName());
+		System.out.println(" Address :"+contact.getAddress());
+		System.out.println(" City :"+contact.getCity());
+		System.out.println(" State :"+contact.getState());
+		System.out.println(" Zip Code :"+contact.getZip());
+		System.out.println(" Phone No :"+contact.getPhoneNo());
+		System.out.println(" Email :"+contact.getEmail());
+		
+	}
+	
 	public static void main(String[] args)
 	{
 		System.out.println(" Welcome to Address Book System ");
+		AddressBookMain addressBookMain = new AddressBookMain();
+		addressBookMain.addContact();
+		addressBookMain.displayContact();
 	}
 }
--- src/com/infogalaxy/addressbooksystem/Contact.java
+++ src/com/infogalaxy/addressbooksystem/Contact.java
@@ -4,10 +4,76 @@ public class Contact
 {
 	private String firstName;
 	private String lastName;
-	private String Address;
-	private String City;
-	private String State;
-	private String Zip;
-	private String PhoneNo;
-	private String Email;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNo;
+	private String email;
+	
+	public void setFirstName(String firstName) 
+	{
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName) 
+	{
+		this.lastName = lastName;
+	}
+	public void setAddress(String address) 
+	{
+		this.address = address;
+	}
+	public void setCity(String city) 
+	{
+		this.city = city;
+	}
+	public void setState(String state) 
+	{
+		this.state = state;
+	}
+	public void setZip(String zip) 
+	{
+		this.zip = zip;
+	}
+	public void setPhoneNo(String phoneNo) 
+	{
+		this.phoneNo = phoneNo;
+	}
+	public void setEmail(String email) 
+	{
+		this.email = email;
+	}
+	
+	public String getFirstName()
+	{
+		return this.firstName;
+	}
+	public String getLastName()
+	{
+		return this.lastName;
+	}
+	public String getAddress()
+	{
+		return this.address;
+	}
+	public String getCity()
+	{
+		return this.city;
+	}
+	public String getState()
+	{
+		return this.state;
+	}
+	public String getZip()
+	{
+		return this.zip;
+	}
+	public String getPhoneNo()
+	{
+		return this.phoneNo;
+	}
+	public String getEmail()
+	{
+		return this.email;
+	}
 }
