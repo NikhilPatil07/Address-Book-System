dbf07e587076a2e17883382c68c3bb9fe0149d3a
NikhilPatil07 <patilnikhil181804@gmail.com>
2024-05-07 17:43:34 +0530

    [ADDED]:Nihil = UC-1 Completed for Create a Contact

---
+++ src/com/infogalaxy/addressbooksystem/Contact.java
@@ -0,0 +1,13 @@
+package com.infogalaxy.addressbooksystem;
+
+public class Contact 
+{
+	private String firstName;
+	private String lastName;
+	private String Address;
+	private String City;
+	private String State;
+	private String Zip;
+	private String PhoneNo;
+	private String Email;
+}
