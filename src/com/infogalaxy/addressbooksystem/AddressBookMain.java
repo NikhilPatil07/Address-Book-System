4ba3bc38c5372ed3ae410031279daa1062b754f8
NikhilPatil07 <patilnikhil181804@gmail.com>
2024-05-07 19:06:34 +0530

    [MODIFIED]: Nikhil - UC-3 Completed for Edit Existing Contact in Address Book

--- src/com/infogalaxy/addressbooksystem/AddressBookMain.java
+++ src/com/infogalaxy/addressbooksystem/AddressBookMain.java
@@ -40,11 +40,44 @@ public class AddressBookMain
 		
 	}
 	
+	void editContact()
+	{
+		System.out.println(" Enter the First Name to Edit Contact ");
+		String firstName = scanner.next();
+		if(firstName.equals(contact.getFirstName()))
+		{
+			System.out.println(" Contact Found ");
+			
+			System.out.println("Enter the First Name :");
+			contact.setFirstName(scanner.next());
+			System.out.println("Enter the Last Name :");
+			contact.setLastName(scanner.next());
+			System.out.println("Enter the Address :");
+			contact.setAddress(scanner.next());
+			System.out.println("Enter the City :");
+			contact.setCity(scanner.next());
+			System.out.println("Enter the State :");
+			contact.setState(scanner.next());
+			System.out.println("Enter the Zip Code :");
+			contact.setZip(scanner.next());
+			System.out.println("Enter the Phone No :");
+			contact.setPhoneNo(scanner.next());
+			System.out.println("Enter the Email :");
+			contact.setEmail(scanner.next());
+		}
+		else 
+		{
+			System.out.println(" Given Name of Contact is Not Found ");
+		}
+	}
+	
 	public static void main(String[] args)
 	{
 		System.out.println(" Welcome to Address Book System ");
 		AddressBookMain addressBookMain = new AddressBookMain();
 		addressBookMain.addContact();
+		addressBookMain.displayContact();
+		addressBookMain.editContact();
 		addressBookMain.displayContact();
 	}
 }
